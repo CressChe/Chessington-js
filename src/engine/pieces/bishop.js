763fb03bc284f7bf3d3d0b1f5918e13da02b4f9d
Cressida Chesworth <Cressida.Chesworth@softwire.com>
2021-06-01 17:37:27 +0100

    bishop moves added

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,35 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        const availableMoves = [];
+        let newColumn = location.col;
+        for (let i = location.row - 1; i >= 0; i--) {
+            newColumn--
+            if (newColumn >= 0)
+                availableMoves.push(Square.at(i, newColumn))
+        }
+        newColumn = location.col;
+        for (let i = location.row + 1; i <= 7; i++) {
+            newColumn++
+            if (newColumn < 8)
+                availableMoves.push(Square.at(i, newColumn))
+        }
+        let newRow = location.row;
+        for (let i = location.col - 1; i >= 0; i--) {
+            newRow++
+            if (newRow < 8)
+                availableMoves.push(Square.at(newRow, i))
+        }
+        newRow = location.row;
+        for (let i = location.col + 1; i <= 7; i++) {
+            newRow--
+            if (newRow >= 0)
+                availableMoves.push(Square.at(newRow, i))
+        }
+        
+       
+
+        return availableMoves;
     }
 }
