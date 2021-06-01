8efd1861343c96587a1d7db5eacbd3afa38d2af3
Cressida Chesworth <Cressida.Chesworth@softwire.com>
2021-06-01 15:13:01 +0100

    rook moves working

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -6,23 +6,21 @@ export default class Pawn extends Piece {
     constructor(player) {
         super(player);
     }
-    
+
     getAvailableMoves(board) {
-        let location = board.findPiece(this)
-       
+        const location = board.findPiece(this)
+
         if (this.player === Player.WHITE) {
-            if (location.row === 1){
+            if (location.row === 1) {
                 return [Square.at(location.row + 2, location.col), Square.at(location.row + 1, location.col)];
-            } 
-            if (location.row > 1)
-                return [Square.at(location.row + 1, location.col)]
+            }
+            return [Square.at(location.row + 1, location.col)]
         }
         if (this.player === Player.BLACK) {
-            if (location.row === 6){
+            if (location.row === 6) {
                 return [Square.at(location.row - 2, location.col), Square.at(location.row - 1, location.col)];
-            } 
-            if (location.row < 6)
-                return [Square.at(location.row - 1, location.col)]
+            }
+            return [Square.at(location.row - 1, location.col)]
         }
     }
 }
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +7,17 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        const availableMoves = [];
+
+        for (let i = 0; i <= 7; i++) {
+            if (i !== location.col)
+                availableMoves.push(Square.at(location.row, i))
+        }
+        for (let j = 0;j <= 7; j++) {
+            if (j !== location.row)
+                availableMoves.push(Square.at(j, location.col))
+        }
+        return availableMoves;
     }
 }
